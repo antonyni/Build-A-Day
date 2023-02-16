67cfa17599cc5f7bd31ac3e0fe4d52b77d1a62f7
Antony <itsokaybehappy@gmail.com>
2023-02-16 18:48:03 -0500

    v.03

--- HomeScreen.js
+++ HomeScreen.js
@@ -1,17 +1,26 @@
 import { View, Button, StyleSheet, TouchableWithoutFeedback } from 'react-native';
+const styles = StyleSheet.create({
+    loginButtonSection: {
+        width: '100%',
+        height: '100%',
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
+function disp({navigation}){
+    <View style={styles.loginButtonSection}>
+                <Button
+                    title="derp"
+                    onPress={() =>
+                        navigation.navigate('Profile', { name: 'Jane' })
+                    }
+                />
+            </View>
+    
+}
 export default function HomeScreen({ navigation }) {
-    const styles = StyleSheet.create({
-        loginButtonSection: {
-            width: '100%',
-            height: '100%',
-            justifyContent: 'center',
-            alignItems: 'center'
-        }
-    })
     return (
-        <TouchableWithoutFeedback  onPress={() =>
-            navigation.navigate('Profile', { name: 'Jane' })
-        }>
+        <TouchableWithoutFeedback onPress={()=>disp({navigation})}>
             <View style={styles.loginButtonSection}>
                 <Button
                     title="Go to Jabe'sn profile"
